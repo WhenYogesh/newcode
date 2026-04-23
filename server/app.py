66a1d058835e047047b8ed05e16b6e829afbfe23
yogesh yogesh <whenyogeshin@gmail.com>
2026-04-23 16:53:38 +0530

    fix2

--- server/app.py
+++ server/app.py
@@ -34,4 +34,4 @@ def root():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "10000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=port)
